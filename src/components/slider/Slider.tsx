64f288e0aa748d66c221ab2f8de9d8c1724ec401
Elahe Narimani <elahe.narimani@gmail.com>
2024-04-23 21:41:07 +0330

    fix slider

--- src/components/slider/Slider.tsx
+++ src/components/slider/Slider.tsx
@@ -27,17 +27,20 @@ function Slider(){
             <img alt="child" src='https://s8.uupload.ir/files/screenshot_(2)_u41a.png' className='w-full h-full' />
         </SwiperSlide>
         <SwiperSlide className='w-full h-[100vh]'>
+            <img alt="child" src='https://s8.uupload.ir/files/8643a805cd39455a4e8e45857f36b644c72c0121_1713701662_gg4d.png' className='w-full h-full' />
+        </SwiperSlide>
+        <SwiperSlide className='w-full h-[100vh]'>
           <img alt="shose" src='https://s8.uupload.ir/files/screenshot_(4)_zzvb.png' className='w-full h-full' />
         </SwiperSlide>
         <SwiperSlide className='w-full h-[100vh]'>
           <img alt="skinCare" src='https://s8.uupload.ir/files/screenshot_(6)_ocyu.png' className='w-full h-full' />
         </SwiperSlide>
-        {/* <SwiperSlide className='w-full h-[100vh]'>
-          <img alt="women" src='https://s8.uupload.ir/files/screenshot_(9)_u47c.png' className='w-full h-full' />
+         <SwiperSlide className='w-full h-[100vh]'>
+          <img alt="women" src='https://s8.uupload.ir/files/2c627a1efeabeeb2d68a4740aad14b9ccd33235f_1713701900_jj7l.png' className='w-full h-full' />
         </SwiperSlide>
         <SwiperSlide className='w-full h-[100vh]'>
           <img alt="women2" src='https://s8.uupload.ir/files/screenshot_(7)_r0p7.png' className='w-full h-full' />
-        </SwiperSlide> */}
+        </SwiperSlide> 
       </Swiper>
     )
 }
