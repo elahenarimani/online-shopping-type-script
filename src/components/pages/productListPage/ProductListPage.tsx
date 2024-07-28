5ca023daa08f5f20472ffe625ef1735c1eff0ece
Elahe Narimani <elahe.narimani@gmail.com>
2024-07-28 21:26:27 +0330

    add some code

--- src/components/pages/productListPage/ProductListPage.tsx
+++ src/components/pages/productListPage/ProductListPage.tsx
@@ -1,31 +1,65 @@
-import { Params, useParams } from 'react-router-dom';
-import { useContext } from 'react';
-import {ProductListContext} from "../../siteSetting/SiteSetting"
-function ProductListPage(){
+import { Link, Params, useParams } from "react-router-dom";
+import { useContext } from "react";
+import { ProductListContext } from "../../siteSetting/SiteSetting";
+import { IoMdHeartEmpty } from "react-icons/io";
+import { useDispatch, useSelector } from "react-redux";
+import { addBuyCart } from "../../redux/reducers/buyCart/buyCartReducer";
+interface IState {
+  id: string | number;
+  count: number;
+}
+function ProductListPage() {
   const { categoryName } = useParams<Params>();
   const productListX = useContext(ProductListContext);
-  const filteredProducts = productListX?.productList.filter(product => product.head_category=== categoryName);
-    return( 
-        <div className='max-w-full h-full m-auto ml-[40px] mr-[40px] flex justify-between items-center gap-[5px]'>
-         
-         {/* {filteredProducts?.map((item) => (
+  const buyCarty: IState[] = useSelector((state: any) => state.buyCarty);
+  const params = useParams();
+  const filteredProducts = productListX?.productList.filter(
+    (product) => product.head_category === categoryName
+  );
+  const dispatch: Function = useDispatch();
+
+  return (
+    <div className="max-w-full h-full m-auto ml-[40px] mr-[40px] flex justify-between items-center gap-[5px]">
+      {/* {filteredProducts?.map((item) => (
           <div key={item.id} className='w-full h-full'>
             <h3>{item.name}</h3>
             <img src={item.image[0]} alt='{product.name}'  />
             <h3>{item.price}</h3>
             </div>
             ))} */}
-            <div className='w-1/5 bg-red-700 p-[15px]'>
-           فیلتر کشویی
-            </div>
-            <div className='w-4/5 bg-gray-800  flex justify-between items-center '>
-              <div className='w-1/5 border-solid border-[#E6E6E6]  bg-gray-800' > hgjhgyhg</div>
-              <div className='w-1/5  border-solid border-[#E6E6E6]  bg-gray-800' > hgjhgyhg</div>
-              <div className='w-1/5  border-solid border-[#E6E6E6]  bg-gray-800' > hgjhgyhg</div>
-              <div className='w-1/5  border-solid border-[#E6E6E6]  bg-gray-800' > hgjhgyhg</div>
-            </div>
-            
+      <div className="w-2/5 bg-red-700">فیلتر کشویی</div>
+     <div className=" grid  grid-cols-4 gap-[15px]">
+     {filteredProducts?.map((item) => (
+        <Link to={`/checkout/cart/`}>
+                  
+        <div key={item.id} className=" w-full">
+          <div className="w-full  border-solid border-[#E6E6E6] border-[2px] rounded-[5px]  flex flex-col">
+            <img src={item.image[0]} alt="photo" className="w-full h-full" />
+            <p className="mr-[10px] mb-[50px]">{item.name}</p>
+            <p className="mr-[10px] mb-[50px]">{item.price}</p>
+            {/* <div className="w-full flex flex-row justify-between items-center gap-[5px]">
+              <div className="w-full">
+                <Link to={`/checkout/cart/`}>
+                  <button
+                    className=" w-full h-[38px]  text-white bg-[#A72F3B] text-center pt-[11px] pb-[11px] rounded-[5px] "
+                    onClick={() => dispatch(addBuyCart({ id: params.id }))}
+                  >
+                    <p>رفتن به سبد خرید</p>
+                  </button>
+                </Link>
+              </div>
+              <div className="w-[38px] h-[38px] text-[24px] cursor-pointer border-[1px] border-[#A72F3B] text-center pt-[11px] pb-[11px] pl-[11px] pr-[11px] rounded-[5px]">
+                <IoMdHeartEmpty color="#A72F3B" />
+              </div>
+            </div> */}
+          </div>
+          
+        
         </div>
-    )
+        </Link>
+      ))}
+     </div>
+    </div>
+  );
 }
 export default ProductListPage;
